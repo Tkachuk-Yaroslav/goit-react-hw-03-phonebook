8e2bdee14e068ac5ed5e1d31ca28e1f0171decd5
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-09-12 16:52:56 +0300

    Переніс сюди друге дз

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,25 @@
+import css from './ContactList.module.css';
+
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul>
+      {contacts.map(contact => {
+        return (
+          <li key={contact.id} className={css.item}>
+            <h2 className={css.title}>{contact.name}</h2>
+            <h2 className={css.number}>{contact.number}</h2>
+            <button
+              className={css.delete}
+              onClick={() => onDeleteContact(contact.id)}
+              type="button"
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ContactList;
